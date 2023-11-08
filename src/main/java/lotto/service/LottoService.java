250d94d35a2e1619779b46e5bc5891629fb85c59
hongjungin <jungini0000@gmail.com>
2023-11-08 22:04:38 +0900

    refactor : 리스트 형식으로 반환값 변경

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -5,13 +5,15 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class LottoService {
-	
+
 	private static final int MIN_LOTTO_NUMBER = 1;
 	private static final int MAX_LOTTO_NUMBER = 45;
 	private static final int LOTTO_NUMBERS_COUNT = 6;
-    
-    public List<Integer> generateLottoNumbers() {
-        return Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER, LOTTO_NUMBERS_COUNT);
-    }
+
+	public List<Integer> generateLottoNumbers() {
+		List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER,
+				LOTTO_NUMBERS_COUNT);
+		return numbers.stream().toList();
+	}
 
 }
