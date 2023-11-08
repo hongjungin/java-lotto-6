f0a681187298117a40da2aa88d460a6318247f63
hongjungin <jungini0000@gmail.com>
2023-11-08 22:12:22 +0900

    refactor : InputView 에서 input 만 하기로 수정

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -7,9 +7,6 @@ import camp.nextstep.edu.missionutils.Console;
 import lotto.validator.Validator;
 
 public class InputView {
-	private static final String INPUT_MONEY_AMOUNT = "구입금액을 입력해 주세요.";
-	private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
-	private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
 
 	private final Validator<String> InputValidator;
 	private final Validator<String> BonusNumberValidator;
@@ -23,21 +20,18 @@ public class InputView {
 	}
 
 	public int inputMoneyAmount() {
-		System.out.println(INPUT_MONEY_AMOUNT);
 		String money = Console.readLine();
 		InputValidator.validate(money);
 		return Integer.parseInt(money);
 	}
 
 	public List<Integer> inputWinningNumber() {
-		System.out.println(INPUT_WINNING_NUMBER);
 		String numbers = Console.readLine();
 		LottoNumbersValidator.validate(numbers);
 		return toIntegerList(numbers);
 	}
 
 	public int inputBonusNumber() {
-		System.out.println(INPUT_BONUS_NUMBER);
 		String bonusNumber = Console.readLine();
 		BonusNumberValidator.validate(bonusNumber);
 		return Integer.parseInt(bonusNumber);
