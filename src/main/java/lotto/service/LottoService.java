4b7ceff662f75ae55097703c51d955e504b37e91
hongjungin <jungini0000@gmail.com>
2023-11-08 22:21:13 +0900

    feat : OutputView 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,7 @@
 package lotto.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -10,10 +11,10 @@ public class LottoService {
 	private static final int MAX_LOTTO_NUMBER = 45;
 	private static final int LOTTO_NUMBERS_COUNT = 6;
 
-	public List<Integer> generateLottoNumbers() {
+	public String generateLottoNumbers() {
 		List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER,
 				LOTTO_NUMBERS_COUNT);
-		return numbers.stream().toList();
+		return numbers.stream().map(String::valueOf).collect(Collectors.joining(", ", "[", "]"));
 	}
 
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,5 +1,47 @@
 package lotto.view;
 
-public class OutputView {
+import java.util.List;
 
+public class OutputView {
+	private static final String INPUT_MONEY_AMOUNT = "구입금액을 입력해 주세요.";
+	private static final String INPUT_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+	private static final String INPUT_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+	private static final String PURCHASED_LOTTO_COUNT = "%d개를 구매했습니다.";
+
+	public void moneyAmount() {
+		System.out.println(INPUT_MONEY_AMOUNT);
+	}
+
+	public void printLottoCount(int lottoCount) {
+		System.out.printf((PURCHASED_LOTTO_COUNT) + "%n", lottoCount);
+	}
+
+	public void printLottoNumbers(List<Integer> numbers) {
+		System.out.print("[");
+		for (int i = 0; i < numbers.size(); i++) {
+			System.out.print(numbers.get(i));
+			printComma(numbers, i);
+		}
+		System.out.println("]");
+	}
+
+	private void printComma(List<Integer> numbers, int i) {
+		if (i != numbers.size() - 1) {
+			System.out.print(", ");
+		}
+
+	}
+
+	public void winningNumbers() {
+		System.out.println(INPUT_WINNING_NUMBER);
+	}
+
+	public void bonusNumber() {
+		System.out.println(INPUT_BONUS_NUMBER);
+	}
+
+	public void printStatic() {
+		System.out.println("당첨 통계");
+		System.out.println("---");
+	}
 }
