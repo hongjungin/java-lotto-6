22f9a469fdbee5b463b2b93f343cd4e7077a37d2
hongjungin <jungini0000@gmail.com>
2023-11-08 19:44:53 +0900

    feat : 로또 구입 금액 예외 처리

---
+++ src/main/java/lotto/validator/InputMoneyValidator.java
@@ -0,0 +1,28 @@
+package lotto.validator;
+
+public class InputMoneyValidator implements Validator<String> {
+
+	@Override
+	public void validate(String input) {
+
+	}
+
+	private void validateNaturalNumber(String input) {
+		try {
+			int money = Integer.parseInt(input);
+			if (money <= 0) {
+				throw new IllegalArgumentException("[ERROR] 입력된 값이 자연수가 아닙니다.");
+			}
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("[ERROR] 입력된 값이 숫자가 아닙니다.");
+		}
+	}
+
+	public void validateMultipleOfThousand(String input) {
+		int money = Integer.parseInt(input);
+		if (money % 1000 != 0) {
+			throw new IllegalArgumentException("[ERROR] 로또 구입 금액은 1000 단위여야 합니다.");
+		}
+	}
+
+}
---
+++ src/main/java/lotto/validator/Validator.java
@@ -0,0 +1,5 @@
+package lotto.validator;
+
+public interface Validator<V> {
+	void validate(V check);
+}
